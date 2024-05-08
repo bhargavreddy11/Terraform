b5c1ade73d1969b37ac589201143caf07a9f482b
bhargavreddy11 <bhargavr549@gmail.com>
2024-05-08 15:56:38 +0000

    My VPC module

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-resource "local_file" "foo" {
- content = "Bhargav Devops Engineer"
- filename = "./file1.txt"
- }
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c3219cee65713809619a735f49793e79e89ce6136f2d8f9fd33b8f95499b21d
+size 44
